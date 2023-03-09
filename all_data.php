bc4924165b7e72ba9074646c9c48d5e66fe5d8a3
Nico <konde2@yandex.ru>
2023-03-09 11:59:55 +0300

    Update all_data.php

--- all_data.php
+++ all_data.php
@@ -1,39 +1,17 @@
 <?php
 
-// Функция добавляет отмеченный пункт в плане питания или удаляет его
+// Функия запрашивает у базы данных отмеченные пукнты плана питания
 
-if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || $_SERVER['HTTP_X_REQUESTED_WITH'] != 'XMLHttpRequest') {
-    return;
-} // проверяем это Ajax запрос, если да - то ловим его
+$user_id = $modx->user->id; // Получаем id пользователя, который находится в личном кабинете
 
-if ($_POST['add_item']) { // Пользователь отмечает галочкой, что он выполнил пункт плана(позавтракал, к примеру)
+$alias = $modx->resource->get('alias');
 
-    $user_id = $modx->user->id; // получаем ID пользователя
+$output = '';
 
-    $alias = $modx->resource->get('alias');
+$sql = "SELECT `stage` FROM `modx_guide` WHERE `user_id` = '" . $user_id . "' AND `day` = '" . $alias . "'";
 
-    $add_item = $_POST['add_item']; // получаем отмеченный пункт
-
-    // Добавляем в таблицу пользователя, день плана питания, отмеченный пункт(завтрак, обед или ужин) и время клика (Всё для статистики)
-
-    $sql2 = $modx->query("INSERT INTO `modx_guide`(`user_id`, `day`, `stage`, `click_time`) VALUES ('" . $user_id . "','" . $alias . "','" . $add_item . "', NOW())");
-
-    die($sql2); // отключаемся от БД
-
+foreach ($modx->query($sql) as $row) { // получаем массив результатов и добавляем отмеченные пункты на текущую страницу
+    $output .= '<div class="' . $row['stage'] . '"></div>';
 }
 
-// Пользователь решил убрать галочку с отмеченного пункта
-
-if ($_POST['delete_item']) {
-
-    $user_id = $modx->user->id; // получаем ID пользователя
-
-    $alias = $modx->resource->get('alias');
-
-    $delete_item = $_POST['delete_item']; // получаем отмеченный пункт
-
-    $sql2 = $modx->query("DELETE FROM `modx_guide` WHERE `user_id` = '" . $user_id . "' AND `day` = '" . $alias . "' AND `stage` = '" . $delete_item . "'"); // удаляем из БД
-
-    die($sql2); // отключаемся от БД
-
-}
+return $output;
